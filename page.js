e42b0322ba4dc7f81147496bb013a349c80daea8
vishnugopy <vishnugoby0010@gmail.com>
2021-01-25 21:52:08 +0100

    😍

--- page.js
+++ page.js
@@ -1,5 +1,6 @@
 let bar = document.querySelector("#main");
 let logo = document.querySelector(".logo");
+let main = document.querySelector("main");
 let space = bar.offsetTop;
 
 
@@ -7,10 +8,12 @@ function helloBuddy() {
   if (window.scrollY >= space) {
     bar.classList.add("newnav");
     logo.style.maxWidth = "10%";
+    main.style.paddingTop = "10%";
   }
   else {
     bar.classList.remove("newnav");
     logo.style.maxWidth = "0";
+    main.style.paddingTop = "5%";
   }
 }
 
